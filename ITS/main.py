357457680385d53ae5141caf8f8fa5336282e5f6
Marcolindo23 <159887360+Marcolindo23@users.noreply.github.com>
2024-05-16 15:44:51 +0200

    Delete ITS directory

--- ITS/main.py
+++
@@ -1,8 +0,0 @@
-from room import Room
-from building import Building
-
-r = Room(name = "213", floor = 2, num_seats = 30)
-print(r)
-print("#" * 50)
-b = Building(name = "SMI", address = "Via della Sierra Nevada 60", floors = 5)
-print(len(b.get_rooms()))
