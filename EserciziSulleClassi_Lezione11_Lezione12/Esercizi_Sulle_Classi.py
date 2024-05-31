1abc9c93ca64e9ea4f0f9b9a155f23ce9500a761
Marcolindo23 <marco.giusepponi1@gmail.com>
2024-05-31 16:17:02 +0200

    esercizi sulle classi lezione 11 e 12 aggiornato

--- EserciziSulleClassi_Lezione11_Lezione12/Esercizi_Sulle_Classi.py
+++ EserciziSulleClassi_Lezione11_Lezione12/Esercizi_Sulle_Classi.py
@@ -35,28 +35,6 @@ class Cinema:
                 return
         print(f"Errore: film '{Titolo_Film}' non trovato.")
 
-# Test case
-def test_cinema():
-    # Configurazione delle sale
-    film1 = Film("Film A", 120)
-    film2 = Film("Film B", 90)
-    
-    sala1 = Sala(1, film1, 100)
-    sala2 = Sala(2, film2, 50)
-    
-    cinema = Cinema()
-    cinema.aggiungi_sala(sala1)
-    cinema.aggiungi_sala(sala2)
-    
-    # Cliente prenota posti per un film
-    cinema.prenota_film("Film A", 10)  # Expected: "10 posti prenotati per il film 'Film A' in Sala 1."
-    cinema.prenota_film("Film B", 60)  # Expected: "Errore: non ci sono abbastanza posti disponibili per il film 'Film B' in Sala 2."
-    cinema.prenota_film("Film C", 10)  # Expected: "Errore: film 'Film C' non trovato."
-    cinema.prenota_film("Film A", 90)  # Expected: "Errore: non ci sono abbastanza posti disponibili per il film 'Film A' in Sala 1."
-    cinema.prenota_film("Film A", 50)  # Expected: "50 posti prenotati per il film 'Film A' in Sala 1."
-
-test_cinema()
-
 
 	
 ##############################################################################################################################################################
@@ -93,32 +71,3 @@ class Magazzino:
         for prodotto in self.prodotti:
             print(f"Prodotto: {prodotto.nome}, Quantità: {prodotto.quantità}")
 
-# Test case
-magazzino = Magazzino()
-
-# Creazione dei prodotti
-prodotto1 = Prodotto("Prodotto1", 10)
-prodotto2 = Prodotto("Prodotto2", 20)
-prodotto3 = Prodotto("Prodotto3", 0)
-
-# Aggiunta dei prodotti al magazzino
-magazzino.aggiungi_prodotto(prodotto1)
-magazzino.aggiungi_prodotto(prodotto2)
-magazzino.aggiungi_prodotto(prodotto3)
-
-# Ricerca di un prodotto
-nome_prodotto_da_cercare = "Prodotto2"
-prodotto_trovato = magazzino.cerca_prodotto(nome_prodotto_da_cercare)
-if prodotto_trovato:
-    print(f"Trovato: {prodotto_trovato.nome} con quantità {prodotto_trovato.quantità}")
-else:
-    print(f"Il prodotto {nome_prodotto_da_cercare} non è stato trovato.")
-
-# Verifica disponibilità dei prodotti
-print(magazzino.verifica_disponibilità("Prodotto1"))
-print(magazzino.verifica_disponibilità("Prodotto2"))
-print(magazzino.verifica_disponibilità("Prodotto3"))
-print(magazzino.verifica_disponibilità("Prodotto4"))
-
-# Mostra tutti i prodotti
-magazzino.mostra_tutti_i_prodotti()
